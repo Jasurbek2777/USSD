e5ee51057089b28a243c0478a6bcdd93f38d04a1
Jasurbek2777 <jasurbekoripov2777@gmail.com>
2021-09-08 17:14:11 +0500

    Lat commit

--- app/src/main/java/uz/juo/ussd/ui/home/HomeFragment.kt
+++ app/src/main/java/uz/juo/ussd/ui/home/HomeFragment.kt
@@ -1,17 +1,13 @@
 package uz.juo.ussd.ui.home
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.*
-import android.view.animation.AnimationUtils
-import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.firestore.FirebaseFirestore
 import uz.juo.ussd.MainActivity
